165ed685251ad57f5c72768734726a11ebddd128
Shane <1163183531@qq.com>
2020-08-02 14:08:43 +0800

    学习字符串

---
+++ Str.py
@@ -0,0 +1,10 @@
+title = '我喜欢的名言'  # 使用单引号 只能一行一行
+mot_cn = "命运给予我们的" \
+         "不是失望之酒"  # 使用双引号 只能一行一行
+mot_en = '''our destiny offers not the cup ''' \
+         '''of  despair 
+but the chance of opportunity'''  # 使用三引号 可以多行
+
+print(title)
+print(mot_cn)
+print(mot_en)
