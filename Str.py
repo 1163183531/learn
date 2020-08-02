99cc52b23b876ec67dd8d02f949ebdf6784d15e5
Shane <1163183531@qq.com>
2020-08-02 14:26:56 +0800

    学习用字符串来打出坦克的

--- Str.py
+++ Str.py
@@ -1,10 +1,16 @@
 title = '我喜欢的名言'  # 使用单引号 只能一行一行
-mot_cn = "命运给予我们的" \
-         "不是失望之酒"  # 使用双引号 只能一行一行
+mot_cn = "命运给予我们的\
+不是失望之酒"  # 使用双引号 只能一行一行
+mot_cn2 = "命运给予我们的" \
+          "不是失望之酒"
+mot_cn3 = ("命运给予我们的"
+           "不是失望之酒")
 mot_en = '''our destiny offers not the cup ''' \
          '''of  despair 
 but the chance of opportunity'''  # 使用三引号 可以多行
 
 print(title)
 print(mot_cn)
+print(mot_cn2)
+print(mot_cn3)
 print(mot_en)
